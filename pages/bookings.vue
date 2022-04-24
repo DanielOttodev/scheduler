13952e1cc633be66f6ba90192b38244b59342108
otto281 <otto_281@hotmail.com>
2022-04-24 10:19:19 +1000

    First Commit

---
+++ pages/bookings.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row>
+  
+    <v-col class="text-center">
+        <h1>Bookings</h1>
+          <Calendar/>
+    </v-col>
+  </v-row>
+
+</template>
+
+<script>
+export default {
+  name: 'InspirePage',
+}
+</script>
